835a002c68598814f52e4f4126a310a53de57112
鄭凱銘 <privatorcheng@hotmail.com>
2016-03-17 12:27:06 +0800

    修改HarryPotterSaleHandler.GetBookOrderPrice後, Scenario 6測試通過

--- BookSale/HarryPotterSaleHandler.cs
+++ BookSale/HarryPotterSaleHandler.cs
@@ -10,33 +10,53 @@ namespace BookSale
         {
             double orderPrice = 0;
             int orderCnt = 0;
-            foreach (BookOrderModel bo in boList)
+
+            while (! NoBookInOrderList(boList))
             {
-                BookDataModel b = _BookList.Where(x => x.BookID == bo.BookID).FirstOrDefault();
-                if (b != null) orderPrice += bo.OrderAmt * b.BookPrice;
-                orderCnt += 1;
+                double tmpOrderPrice = 0;
+                foreach (BookOrderModel bo in boList)
+                {
+                    BookDataModel b = _BookList.Where(x => x.BookID == bo.BookID).FirstOrDefault();
+                    if (b != null && bo.OrderAmt != 0)
+                    {
+                        tmpOrderPrice += b.BookPrice;
+                        bo.OrderAmt -= 1;
+                    }
+                    orderCnt += 1;
+                }
+                double dDiscount;
+                switch (orderCnt)
+                {
+                    case 2:
+                        dDiscount = 0.95;
+                        break;
+                    case 3:
+                        dDiscount = 0.9;
+                        break;
+                    case 4:
+                        dDiscount = 0.8;
+                        break;
+                    case 5:
+                        dDiscount = 0.75;
+                        break;
+                    default:
+                        dDiscount = 1;
+                        break;
+                }
+                tmpOrderPrice = tmpOrderPrice * dDiscount;
+                orderPrice += tmpOrderPrice;
             }
-            double dDiscount;
-            switch (orderCnt)
-            {
-                case 2:
-                    dDiscount = 0.95;
-                    break;
-                case 3:
-                    dDiscount = 0.9;
-                    break;
-                case 4:
-                    dDiscount = 0.8;
-                    break;
-                case 5:
-                    dDiscount = 0.75;
-                    break;
-                default:
-                    dDiscount = 1;
-                    break;
-            }
-            orderPrice = orderPrice * dDiscount;
+
             return orderPrice;
+        }
+        private bool NoBookInOrderList(List<BookOrderModel> boList)
+        {
+            bool f = true;
+            foreach(BookOrderModel bo in boList)
+            {
+                if (bo.OrderAmt != 0) f = false;
+            }
+            return f;
         }
     }
 }
